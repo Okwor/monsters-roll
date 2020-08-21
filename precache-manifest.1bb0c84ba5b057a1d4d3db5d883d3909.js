515863d8ef5fac746b60432d6c192556732e44c7
Your Name <https://github.com/Okwor/ecm>
2020-08-21 13:54:51 +0100

    Updates

---
+++ precache-manifest.1bb0c84ba5b057a1d4d3db5d883d3909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedda4326a048cd56fb9323c58ba6b5e",
+    "url": "/monsters-roll/index.html"
+  },
+  {
+    "revision": "79438d0a0ff5bcf4a0fc",
+    "url": "/monsters-roll/static/css/main.520d3f22.chunk.css"
+  },
+  {
+    "revision": "a3cdf1c7e60fa597e9e3",
+    "url": "/monsters-roll/static/js/2.229dafa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roll/static/js/2.229dafa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79438d0a0ff5bcf4a0fc",
+    "url": "/monsters-roll/static/js/main.9f352047.chunk.js"
+  },
+  {
+    "revision": "164dc0b8f85b6ec4b15b",
+    "url": "/monsters-roll/static/js/runtime-main.50baca05.js"
+  }
+]);
